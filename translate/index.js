e0ecf17395109f3a623b6c92d716fa2999e296d8
villas <starkcoder@sina.com>
2024-01-26 00:44:18 +0800

    feat: 翻译命令 ti-tl, v0.1.1

---
+++ translate/index.js
@@ -0,0 +1,29 @@
+const { exec } = require("child_process");
+const { showErrorExist } = require("../common/tool");
+
+function parseArgsConfig() {
+  const args = process.argv.slice(2);
+  if (args.length !== 1) {
+    showErrorExist("(only) one word plz..");
+  }
+  const toTransWord = args[0].trim();
+  if (!toTransWord) {
+    showErrorExist("don't be blank plz..");
+  }
+  return toTransWord;
+}
+
+function toBrowserTranslate() {
+  const toTransWord = parseArgsConfig();
+  let url = "https://fanyi.baidu.com/#zh/en/";
+  const letterREG = /^[a-zA-Z]/;
+  if (letterREG.test()) {
+    url = "https://fanyi.baidu.com/#en/zh/";
+  }
+
+  // https://stackoverflow.com/questions/8500326/how-to-use-nodejs-to-open-default-browser-and-navigate-to-a-specific-url
+  var start = process.platform == "darwin" ? "open" : process.platform == "win32" ? "start" : "xdg-open";
+  exec(`${start} ${url + toTransWord}`);
+}
+
+toBrowserTranslate();
